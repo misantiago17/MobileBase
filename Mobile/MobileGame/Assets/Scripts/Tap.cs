62262674c0502605541c77e04a95ae22e427d7ce
Michelle <michellesantiagobeadle@hotmail.com>
2019-10-23 18:51:13 -0300

    Tap

--- Mobile/MobileGame/Assets/Scripts/Tap.cs
+++ Mobile/MobileGame/Assets/Scripts/Tap.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class Tap : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    // OBS: limitar o drag em favor do tap, se tiver segurando tempo suficiente vira drag
+    // Talvez considerar tap se o objecto não se mover
+
+    // Tempo maximo até o player soltar o botão para que ele considere como tap
+    public float tapTime = 0.15f;
+
+    private bool tapped = false;
+    private float timer = 0;
+
+    private void OnMouseDown() {
+        tapped = true;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnMouseUp() {
+
+        tapped = false;
+
+        if (timer <= tapTime) {
+            // Do action
+            Debug.Log("Fiz um tap");
+        }
+
+        timer = 0;
     }
+
+    private void Update() {
+
+        if (tapped) {
+            timer += Time.deltaTime;
+        }
+
+    }
+
 }
